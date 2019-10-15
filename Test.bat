99bff4cc0266100dcba02d32eba612151c90cd7e
Panachainy <panachai.ny@gmail.com>
2019-10-16 00:25:35 +0700

    update test.bat for merge

--- Test.bat
+++ Test.bat
@@ -1,4 +1,6 @@
-dotnet test /p:CollectCoverage=true /p:MergeWith=../coverage/lcov.json /p:CoverletOutput=../coverage/lcov /p:CoverletOutputFormat='json%2copencover' CodeCoverageDotnetCore.sln
+del coverage /F /Q
+del coveragereport /F /Q
+dotnet test /p:CollectCoverage=true /p:MergeWith=../coverage/lcov.json /p:CoverletOutput=../coverage/lcov /p:CoverletOutputFormat="json%%2copencover" CodeCoverageDotnetCore.sln
 reportgenerator "-reports:coverage/lcov.opencover.xml" "-targetdir:coveragereport"
 start coveragereport/index.htm
 REM base on cmd now not work
