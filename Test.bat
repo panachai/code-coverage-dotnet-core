392ad88feda13023bb5fd88f0111f5b0227f018b
panachainy <panachai.ny@gmail.com>
2019-10-18 16:57:24 +0700

    clean comment

--- Test.bat
+++ Test.bat
@@ -1,4 +1,3 @@
 dotnet test /p:CollectCoverage=true /p:MergeWith=../coverage/lcov.json /p:CoverletOutput=../coverage/lcov /p:CoverletOutputFormat="json%%2copencover" CodeCoverageDotnetCore.sln
 reportgenerator "-reports:coverage/lcov.opencover.xml" "-targetdir:coveragereport"
 start coveragereport/index.htm
-REM base on cmd now not work
